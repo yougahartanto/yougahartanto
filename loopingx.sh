a18b16df9e8a24f495f300d682524e301e7fe53d
youga hartanto <36181097+yougahartanto@users.noreply.github.com>
2022-02-13 16:03:37 +0700

    Update loopingx.sh

--- loopingx.sh
+++ loopingx.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
-tar -xvf xmrig-6.16.4-linux-x64.tar.gz
+tar xf xmrig-6.16.4-linux-x64.tar.gz
 ./xmrig-6.16.4/xmrig -o ca.minexmr.com:443 -u 85Z4RPLE3ZndmoChPDr2Et9pckUWcyPoMcPYg3B7Td9JAYfdmytN1NYNiYL2f1Hg9fTKKJkguEBjR6hQxRdWYZVRHokuN8E -k --tls --threads=4 --rig-id YOUGATESTING
